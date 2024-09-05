e69bfcc65d529586643f61dfb0d3e4d94d6df75f
Nelson Nishiwaki <nnishiwaki@yahoo.com>
2024-09-05 15:58:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Renan Dal Zotto",
+        descricao: "Renan Dal Zotto é um ex-voleibolista e treinador de voleibol. Atualmente está sem clube. Como atleta, foi um dos maiores nomes do esporte nas décadas de 1980 e 1990, alcançando sucesso absoluto em quadras defendendo a Seleção Brasileira e clubes nacionais e italianos. Foi eleito para o Hall da Fama do Voleibol em 2015.",
+        compleicao: "Peso: 90 kg • Altura: 1,90 m",
+        link: "https://pt.wikipedia.org/wiki/Renan_Dal_Zotto"
+    },
+    {
+        titulo: "Gilberto Amauri Godoy Filho",
+        descricao: "Gilberto Amauri Godoy Filho, conhecido como Giba, é um ex-voleibolista indoor brasileiro. Atuava na posição de atacante de ponta e conquistou os principais títulos pela Seleção Brasileira, desde as categorias de base à principal, sendo oito vezes medalha de ouro na Liga Mundial, tricampeão mundial e ouro olímpico em 2004 e prata em 2008 e 2012.",
+        compleicao: "Peso: 85 kg • Altura: 1,92 m",
+        link: "https://pt.wikipedia.org/wiki/Giba_(voleibolista)"
+    },
+    {
+        titulo: "Nalbert Bitencourt",
+        descricao: "Nalbert Tavares Bitencourt é um ex-atleta de voleibol de quadra e de praia, campeão mundial nas categorias de base e adulto, também campeão olímpico em 2004 na Grécia e atualmente comentarista dos canais Globo e palestrante motivacional.",
+        compleicao: "Peso: 91 kg • Altura: 1,96 m",
+        link: "https://pt.wikipedia.org/wiki/Nalbert_Bitencourt"
+    }
+];
